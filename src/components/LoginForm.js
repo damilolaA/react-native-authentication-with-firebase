571891a925c729f35322c8059439f8c2ee1235ca
damilolo <adiodamilola@yahoo.com>
2018-04-26 15:30:06 -0700

    call firebase api to signIn user

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Button, Input } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Card, CardSection, Button, Input, Spinner } from './common';
 
 class LoginForm extends Component {
 	constructor() {
 		super();
 		this.state = {
-			text: ''
+			email: '',
+			password: '',
+			error: '',
+			loading: false
 		}
+
+		this.onButtonPress = this.onButtonPress.bind(this);
+	}
+
+	onButtonPress() {
+		this.setState({error: '', loading: true});
+
+		const { email, password } = this.state;
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({error: 'Authentication failed'})
+					});
+			});
+	}
+
+	showButton() {
+		if(this.state.loading) {
+			return <Spinner size="small" />
+		}
+
+		return (
+			<Button whenPressed={this.onButtonPress}>
+				Login
+			</Button>
+		);
 	}
 
 	render() {
@@ -14,21 +47,40 @@ class LoginForm extends Component {
 			<Card>
 				<CardSection>
 					<Input 
-						label={'name'}
-						value={this.state.text}
-						onChangeText={text => {this.setState({ text })}}
+						placeholder="user@gmail.com"
+						label={'Email'}
+						value={this.state.email}
+						onChangeText={email => {this.setState({ email })}}
 					/>
 				</CardSection>
 
-				<CardSection />
-				
 				<CardSection>
-					<Button>
-						Login
-					</Button>
+					<Input
+						secureTextEntry
+						placeholder="password"
+						label={'Password'}
+						value={this.state.password}
+						onChangeText={password => {this.setState({ password })}}
+					/>
+				</CardSection>
+					
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
+				<CardSection>
+					this.showButton();
 				</CardSection>
 			</Card>
 		);
+	}
+}
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
 	}
 }
 
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+	const { inputStyle, textStyle, containerStyle } = styles;
+
 	return (
-		<View>
-			<Text>{label}</Text>
+		<View style={containerStyle}>
+			<Text style={textStyle}>{label}</Text>
 			<TextInput 
+				secureTextEntry={secureTextEntry}
+				placeholder={placeholder}
+				underlineColorAndroid='transparent'
+				autoCorrect={false}
+				style={inputStyle}
 				value={value}
 				onChangeText={onChangeText}
-				style={{ height: 40, width:200, borderColor:'gray' }}
 			/>
 		</View>
 	);
 }
 
 const styles = {
-	InputStyle: {
-		color: #000,
+	inputStyle: {
+		color: '#000',
 		paddingRight: 5,
 		paddingLeft: 5,
 		fontSize: 18, 
 		lineHeight: 23,
 		flex: 2
 	},
-	TextStyle: {},
-	ContainerStyle: {}
+	textStyle: {
+		fontSize: 18,
+		paddingLeft: 20,
+		flex: 1
+	},
+	containerStyle: {
+		height: 40,
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center'
+	}
 }
 
 export { Input }
