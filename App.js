420ec19dad7e25a56bdbbeeb9cc2fa4e94cfea11
damilolo <adiodamilola@yahoo.com>
2018-04-29 22:33:00 -0700

    handle user LogOut button using firbase

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './src/components/common';
+import { Header, CardSection, Button, Spinner } from './src/components/common';
 import firebase from 'firebase';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false
+    }
+  }
 
   componentDidMount() {
     firebase.initializeApp({
@@ -15,13 +21,36 @@ class App extends Component {
       storageBucket: "auth-aecd4.appspot.com",
       messagingSenderId: "981434833484"
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setState({loggedIn:true})
+      } else {
+        this.setState({loggedIn:false})
+      }
+    })
+  }
+
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true: 
+        return (
+          <CardSection>
+            <Button whenPressed={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+        );
+      case false: 
+        return <LoginForm />;
+      default: 
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View >
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
